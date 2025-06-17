f9af22d0fbf69c58eaa8f35610bb82ea17702853
Nakano as a Service <nakanoasaservice@gmail.com>
2025-06-17 12:22:27 +0900

    chore: add uuid58 using crypto.randomUUID to bench

--- benchmark.ts
+++ benchmark.ts
@@ -1,3 +1,4 @@
+import { ALPHABET_LENGTH, BASE58_ALPHABET } from "./alphabet.ts";
 import { uuid58Decode } from "./decode.ts";
 import { uuid58Encode } from "./encode.ts";
 import { uuid58 } from "./uuid58.ts";
@@ -10,6 +11,22 @@ Deno.bench("uuid58Decode", () => {
   uuid58Decode("XDY9dmBbcMBXqcRvYw8xJ2");
 });
 
-Deno.bench("uuid58", () => {
+Deno.bench("uuid58 (using crypto.getRandomValues)", () => {
   uuid58();
 });
+
+Deno.bench("uuid58 (using crypto.randomUUID)", () => {
+  uuid58RandomUUID();
+});
+
+function uuid58RandomUUID(): string {
+  let num = BigInt("0x" + crypto.randomUUID().replace(/-/g, ""));
+  let encoded = "";
+
+  do {
+    encoded = BASE58_ALPHABET[Number(num % ALPHABET_LENGTH)] + encoded;
+    num /= ALPHABET_LENGTH;
+  } while (num > 0n);
+
+  return encoded.padStart(22, BASE58_ALPHABET[0]);
+}
